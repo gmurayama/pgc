98374bf49b65fe91d52b55887f983aa2e8ef62c0
Gustavo Murayama <gmurayama@outlook.com>
2022-03-19 20:54:05 -0300

    reduce number of items in cart

--- scripts/generate_data.ts
+++ scripts/generate_data.ts
@@ -26,33 +26,14 @@ const chance = new Chance();
 const products: Product[] = [];
 const cartItems: CartItem[] = [];
 
-for (let i = 0; i < 200; i++) {
-  products.push({ name: chance.string({ length: 5 }), price: chance.integer({ min: 100, max: 1000 * 100 }) });
+for (let i = 0; i < 250; i++) {
+  products.push({ name: chance.string({ length: 5, pool: 'ABCDEFGHIKLMNOPQRSTUVWXYZ' }), price: chance.integer({ min: 100, max: 1000 * 100 }) });
 }
 
-// cart with 1 items
-let ci = generateCartItems(1, 1, products.length);
-cartItems.push(...ci);
-
-// cart with 3 items
-ci = generateCartItems(1, 3, products.length);
-cartItems.push(...ci);
-
-// cart with 5 items
-ci = generateCartItems(2, 5, products.length);
-cartItems.push(...ci);
-
-// cart with 10 items
-ci = generateCartItems(3, 10, products.length);
-cartItems.push(...ci);
-
-// cart with 20 items
-ci = generateCartItems(4, 20, products.length);
-cartItems.push(...ci);
-
-// cart with 200 items
-ci = generateCartItems(5, 200, products.length);
-cartItems.push(...ci);
+for (let i = 1; i <= products.length; i++) {
+  let ci = generateCartItems(i, i, products.length);
+  cartItems.push(...ci);
+}
 
 fs.writeFileSync(`${__dirname}/../data/products.json`, JSON.stringify(products, null, 2));
 fs.writeFileSync(`${__dirname}/../data/cartItems.json`, JSON.stringify(cartItems, null, 2));
