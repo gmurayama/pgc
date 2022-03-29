a50a19622076ecd24c40ab00dc3f1855ff21c024
Gustavo Murayama <gmurayama@outlook.com>
2022-03-29 09:51:18 -0300

    test

---
+++ scripts/helper/queue.ts
@@ -0,0 +1,56 @@
+interface Node<T> {
+  data: T
+  next: Node<T> | undefined
+}
+
+export class Queue<T> {
+  head: Node<T> | undefined;
+  last: Node<T> | undefined;
+  size: number = 0;
+
+  public enqueue(data: NonNullable<T>): void {
+    const node: Node<T> = {
+      data,
+      next: undefined
+    }
+
+    // list is empty
+    if (this.head === undefined) {
+      this.head = node;
+      this.last = node;
+      return;
+    }
+
+    // list has one item
+    if (this.head.next === undefined) {
+      this.head.next = node;
+      this.last = node;
+    }
+
+    // list has two or more items
+    this.last!.next = node;
+    this.last = node;
+
+    this.size++;
+  }
+
+  public dequeue(): T | undefined {
+    // list is empty
+    if (this.head === undefined) {
+      return undefined;
+    }
+
+    // list has one or more items
+    const { data, next } = this.head;
+    this.head = next;
+
+    // list is now empty after dequeue
+    if (this.head === undefined) {
+      this.last = undefined;
+    }
+
+    this.size--;
+
+    return data;
+  }
+}
--- scripts/run_model_er.ts
+++ scripts/run_model_er.ts
@@ -2,7 +2,15 @@ import products from '../data/seed/products.json';
 import cartItems from '../data/seed/cartItems.json';
 import fs from 'fs/promises';
 import type { ModelERInstance } from '../types/truffle-contracts';
+import TaskManager from './helper/taskManager';
 
+const taskManager = new TaskManager<Truffle.TransactionResponse<any>>({
+  create: <T extends keyof ModelERFunc>(modelER: ModelERInstance, func: T, ...args: Parameters<ModelERFunc[T]>) => {
+    // @ts-ignore (some functions does not have the sendTransaction function in their type annotation)
+    return () => modelER[func].sendTransaction(...args);
+  },
+  maxResourceAvailable: 1000
+})
 const modelERContract = artifacts.require('ModelER');
 
 type ModelERFunc = { [P in keyof ModelERInstance['methods']]: ModelERInstance[P] extends (...args: any[]) => any ? ModelERInstance[P] : never }
@@ -14,10 +22,11 @@ interface Transaction {
 }
 
 module.exports = function (callback) {
+  console.time('run-model-er');
   main()
     .catch(console.error)
     .finally(() => {
-      console.log('finished');
+      console.timeEnd('run-model-er');
       callback();
     })
 }
@@ -26,29 +35,29 @@ async function main() {
 
   const transactions: Array<Transaction> = [];
 
-  console.log(`[${new Date()}] Start populating products`);
+  log(`Start populating products`);
   let tx = await populateProducts(modelER);
   transactions.push(...tx);
-  console.log(`[${new Date()}] End populating products`);
+  log(`End populating products`);
 
-  console.log(`[${new Date()}] Start populating cart items`);
+  log(`Start populating cart items`);
   tx = await populateCartItems(modelER);
   transactions.push(...tx);
-  console.log(`[${new Date()}] End populating cart items`);
+  log(`End populating cart items`);
 
-  console.log(`[${new Date()}] Start deduplicating cart IDs`);
+  log(`Start deduplicating cart IDs`);
   const uniqueCartIds = cartItems
     .reduce((set, cartItem) => set.add(cartItem.cartId), new Set<number>())
-  console.log(`[${new Date()}] End deduplicating cart IDs`);
+  log(`End deduplicating cart IDs`);
 
-  console.log(`[${new Date()}] Start showing cart total price`);
+  log(`Start showing cart total price`);
   for (const cartId of uniqueCartIds) {
     const tx = await showCartTotalPrice(modelER, cartId);
     transactions.push(tx);
   }
-  console.log(`[${new Date()}] End showing cart total price`);
+  log(`End showing cart total price`);
 
-  console.log(`[${new Date()}] Start writing transactions`);
+  log(`Start writing transactions`);
 
   const summarizedTransactions = transactions.map((t) => {
     return { name: t.name, gasUsed: t.tx.receipt.gasUsed };
@@ -63,18 +72,18 @@ async function main() {
   }
 
   await fs.writeFile(
-    `${dirPath}/tx.json`,
+    `${dirPath}/er-model-tx.json`,
     JSON.stringify(transactions, null, 2),
     { flag: 'w' }
   );
 
   await fs.writeFile(
-    `${dirPath}/tx-summarized.json`,
+    `${dirPath}/er-model-tx-summ.json`,
     JSON.stringify(summarizedTransactions, null, 2),
     { flag: 'w' }
   );
   
-  console.log(`[${new Date()}] Start writing transactions`);
+  log(`Start writing transactions`);
 }
 
 async function populateProducts(modelER: ModelERInstance) {
@@ -83,42 +92,71 @@ async function populateProducts(modelER: ModelERInstance) {
   for (let product of products) {
     const transaction = await sendTransaction(modelER, 'addProduct', product.name, product.price);
     transactions.push({ name: 'addProduct', tx: transaction, args: { name: product.name, price: product.price } });
-    console.log(`populate products { name: ${product.name}, price: ${product.price} }`);
+    log(`populate products { name: ${product.name}, price: ${product.price} }`);
   }
 
   return transactions;
 }
 
 async function populateCartItems(modelER: ModelERInstance) {
-  const transactions: Transaction[] = [];
-
   let promises: Promise<Transaction>[] = [];
-  for (let i = 0, cartItem = cartItems[i]; i < cartItems.length; i++, cartItem = cartItems[i]) {
-    const transaction = sendTransaction(modelER, 'addItemToCart', cartItem.cartId, cartItem.productId)
-      .then((t) => {
-        console.log(`${i + 1}/${cartItems.length}: populate cart items { cartId: ${cartItem.cartId}, productId: ${cartItem.productId} }`);
-        return { name: 'addItemToCart', tx: t, args: { cartId: cartItem.cartId, productId: cartItem.productId } } as Transaction;
-      }); 
-    promises.push(transaction);
 
-    if ((i + 1) % 1000 == 0) {
-      transactions.push(...(await Promise.all(promises)));
-      promises = [];
-    }
+  for (let i = 0, cartItem = cartItems[i]; i < cartItems.length; i++, cartItem = cartItems[i]) {
+    const transaction = sendManagedTransaction(modelER, 'addItemToCart', cartItem.cartId, cartItem.productId)
+      .then((t) => {
+        log(`${i + 1}/${cartItems.length}: populate cart items { cartId: ${cartItem.cartId}, productId: ${cartItem.productId} }`);
+        return { name: 'addItemToCart', tx: t, args: { cartId: cartItem.cartId, productId: cartItem.productId } } as Transaction;
+      });
+
+    promises.push(transaction);
   }
 
-  transactions.push(...(await Promise.all(promises)));
+  const tx = await Promise.all(promises);
 
-  return transactions;
+  return tx;
 }
 
 async function showCartTotalPrice(modelER: ModelERInstance, cartId: number) {
   const transaction = await sendTransaction(modelER, 'showTotal', cartId);
-  console.log(`show cart total price { cartId: ${cartId} }`);
+  log(`show cart total price { cartId: ${cartId} }`);
   return { name: 'showTotal', tx: transaction, args: { cartId } } as Transaction;
 }
 
-async function sendTransaction<T extends keyof ModelERFunc>(modelER: ModelERInstance, func: T, ...args: Parameters<ModelERFunc[T]>): Promise<Truffle.TransactionResponse<any>> {
+function sendTransaction<T extends keyof ModelERFunc>(modelER: ModelERInstance, func: T, ...args: Parameters<ModelERFunc[T]>): Promise<Truffle.TransactionResponse<any>> {
   // @ts-ignore (some functions does not have the sendTransaction function in their type annotation)
   return modelER[func].sendTransaction(...args)
+}
+
+function sendManagedTransaction<T extends keyof ModelERFunc>(modelER: ModelERInstance, func: T, ...args: Parameters<ModelERFunc[T]>): Promise<Truffle.TransactionResponse<any>> {
+  return taskManager
+    .run(modelER, func, ...args)
+}
+
+function log(message) {
+  const date = new Date();
+  const hours = formatTime(date.getHours());
+  const minutes = formatTime(date.getMinutes());
+  const seconds = formatTime(date.getSeconds());
+  const milliseconds = formatMilliseconds(date.getMilliseconds());
+  console.log(`[${hours}:${minutes}:${seconds}.${milliseconds}] ${message}`);
+}
+
+function formatTime(time: number) {
+  if (time < 10) {
+    return `0${time}`;
+  }
+
+  return String(time);
+}
+
+function formatMilliseconds(time: number) {
+  if (time < 10) {
+    return `00${time}`;
+  }
+
+  if (time < 100) {
+    return `0${time}`;
+  }
+
+  return String(time);
 }
